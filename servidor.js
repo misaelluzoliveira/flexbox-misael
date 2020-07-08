39c9c55f29166f9801f59ac4a4211f83e3e466cd
Misael Oliveira da Luz <misaeliks@hotmail.com>
2020-07-08 12:21:04 -0300

    exercicios att

---
+++ servidor.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const PORT = 3000;
+app.listen(PORT,()=>console.log(`Servidor rodando ... http://localhost:${PORT}`))
